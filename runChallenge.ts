69aa1f40f60e3ceb2228d010a87687d519889a4b
Hendrik <mail@hendrikwallbaum.de>
2020-12-02 10:57:41 +0100

    ✨ migrate to multi-year structure.

--- runChallenge.ts
+++ runChallenge.ts
@@ -1,20 +1,38 @@
 import * as fs from 'fs'
 import * as path from 'path'
 
+let year = 2020
+
 const run = async () => {
   const challengeString = process.argv[2]
   const dayToRun = parseInt(challengeString.split('-')[0])
   const partToRun = challengeString.split('-')[1] || '1'
 
-  console.log(`Running challenge: Day ${dayToRun} - Part ${partToRun} 🎅`)
+  const yearInput = process.argv[3]
+  if (yearInput) {
+    const parsedYear = parseInt(yearInput, 10)
+    if (!isNaN(parsedYear)) year = parsedYear
+  }
+
+  console.log(
+    `Running challenge: ${year} Day ${dayToRun} - Part ${partToRun} 🎅`
+  )
 
   const challengeFolder = path.resolve(
-    `./Challenges/Day${dayToRun < 10 ? '0' : ''}${dayToRun}`
+    `./challenges/${year}/day${dayToRun < 10 ? '0' : ''}${dayToRun}`
   )
   const solverPath = path.join(challengeFolder, `/day${dayToRun}-${partToRun}`)
   const inputPath = path.join(challengeFolder, '/input.txt')
 
-  const solver = (await import(solverPath)).default
+  let solver
+  try {
+    solver = (await import(solverPath)).default
+  } catch (e) {
+    return console.error(`🚨 No solver found at: ${solverPath}!`)
+  }
+  if (!fs.existsSync(inputPath)) {
+    return console.error(`🚨 No input found at: ${inputPath}!`)
+  }
   const input = fs.readFileSync(inputPath).toString()
 
   const answer = solver(input)
